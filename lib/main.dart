8eb4e6d19d0d10fa049a1961e1f2631f0f65a84e
Aluno Feira <aluno.fsa@senaiba.com.br>
2025-04-07 10:44:25 -0300

    primeiro commit

---
+++ lib/main.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(LoginApp ());
+}
+
+class LoginApp extends StatelessWidget{
+@override
+
+Widget build(BuildContext context){
+return MaterialApp(
+
+debugShowCheckedModeBanner: false,
+home: LoginPage(),
+
+  );
+ }
+}
+
+class LoginPage extends StatelessWidget{
+@override
+
+Widget build(BuildContext context){
+return Scaffold(
+appBar: AppBar (
+   backgroundColor: const Color.fromARGB(255, 54, 197, 154),
+title:Text ('Login'),
+
+),
+body: Padding(
+  padding: EdgeInsets.all(16.0),
+  child: Column(
+    mainAxisAlignment: MainAxisAlignment.center,
+    crossAxisAlignment: CrossAxisAlignment.stretch,
+  children: [
+    Image.asset(
+      'lib/assets/img_login.png',
+       width: 350,
+       height: 250,
+    ),
+
+   SizedBox(height: 40.0),
+
+    TextFormField(
+      decoration: InputDecoration(labelText :'Email',
+      prefixIcon: Icon (Icons.email),
+      border: OutlineInputBorder(
+       borderRadius: BorderRadius.circular(10.0),
+     ),
+    ),
+   ),
+   SizedBox(height: 10.0),
+   TextFormField(
+     obscureText: true,
+      decoration: InputDecoration(labelText :'Senha',
+      prefixIcon: Icon (Icons.password),
+      border: OutlineInputBorder(
+       borderRadius: BorderRadius.circular(10.0),
+
+     ),
+    ),
+   ),
+    SizedBox(height: 32.0),
+    ElevatedButton(
+      onPressed:(){
+
+
+      }, 
+       style: ElevatedButton.styleFrom(
+        backgroundColor: Color.fromARGB(255, 54, 197, 154), //cor do fundo do botao
+        foregroundColor: Colors.white, //cor do texto do botao
+        padding: EdgeInsets.symmetric(horizontal:40, vertical: 15),
+        shape: RoundedRectangleBorder(
+          borderRadius:BorderRadius.circular(20),
+        ),
+       ),        
+       
+      
+      child: Text('Login'),
+    ),
+  ],
+  ),
+ ),
+);
+
+}
+
+}
